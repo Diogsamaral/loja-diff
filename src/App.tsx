a7966d1066ff4ea7d4037c1cfa1c8ccab37c905e
Diogo <diogodamunc@gmail.com>
2025-11-03 09:43:05 -0300

    titulos da pagina

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import CardContainer from "./components/CardContainer";
 function App() {
   return (
     <>
-      <h1>🌌 SUA JORNADA COMEÇA AQUI 🌌</h1>
-      <h2>🚫 COLEÇÃO NO LOGO 🚫</h2>
+      <h1>🌌👽 SUA JORNADA COMEÇA AQUI 👽🌌</h1>
+      <h2>👽🚫 COLEÇÃO NO LOGO 🚫👽</h2>
       <CardContainer />
     </>
   );
