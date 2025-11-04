85e725bb3231a3ed8f69e5051c1e0410f43e54ab
Diogo <diogodamunc@gmail.com>
2025-11-03 21:54:53 -0300

    titulos da pagina

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,17 @@ function App() {
     <>
       <h1>🌌👽 SUA JORNADA COMEÇA AQUI 👽🌌</h1>
       <h3>🚫 COLEÇÃO NO LOGO 🚫</h3>
-
+      <h4>
+        Senhoras e senhoras, terraquios e estraterrestes, apresentamos a vocês
+        nossa primeira coleção, a coleção no logo. As peças não tem logo, apenas
+        nas etiquetas. Peças elegante e com caimento impecável. Claro que a
+        modelagem vai agradar o corpo padrão masculino. Modelo M e G padrão.
+        Tentamos deixar o design da gola mais agradável usando um modelo de gola
+        esporte. Esse design valoriza o caimento e deixa mais formal mesmo
+        estando com uma camisa leve. Calça, tem um corte diferencia, modelagem
+        direto de Londres. Nosso trabalho a princípio foi desenvolver algo
+        fidedigno. Hoje temos um exemplares para oferecer aos hunamos.
+      </h4>
       <CardContainer />
     </>
   );
